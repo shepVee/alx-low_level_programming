121bd1c18d067f24731479a9d59988f6dc086653
shepvee <shepherdmabena@gmail.com>
2022-10-13 16:30:01 -0700

    fixed

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,6 +17,6 @@ int main(void)
 	printf("Size of an int: %zu byte(s)\n", sizeof(a));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(b));
 	printf("Size of long long int: %zu byte(s)\n", sizeof(c));
-	printf("Size of a float: %zu bytes(s)\n", sizeof(f));
+	printf("Size of a float: %zu byte(s)\n", sizeof(f));
 	return (0);
 }
