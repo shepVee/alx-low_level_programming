c3775da6dc96a4516c5506a18b16d09312d9705f
shepvee <shepherdmabena@gmail.com>
2022-10-13 16:23:07 -0700

    stil fixing 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,9 +13,9 @@ int main(void)
 	char d;
 	float f;
 
-	printf("Size of char: %zu bytes\n", sizeof(d));
-	printf("Size of an int: %zu bytes\n", sizeof(a));
-	printf("Size of a long int: %zu bytes\n", sizeof(b));
+	printf("Size of char: %zu byte\n", sizeof(d));
+	printf("Size of an int: %zu byte\n", sizeof(a));
+	printf("Size of a long int: %zu byte\n", sizeof(b));
 	printf("Size of long long int: %zu byte\n", sizeof(c));
 	printf("Size of a float: %zu byte\n", sizeof(f));
 	return (0);
