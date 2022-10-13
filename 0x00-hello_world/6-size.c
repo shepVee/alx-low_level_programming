fbc0ab7c25b8fabe5bdb8aa85358f0044cca5b9c
shepvee <shepherdmabena@gmail.com>
2022-10-13 15:38:53 -0700

    Create size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio>
+
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	long int b;
+	long long int c;
+	char d;
+	float e;
+
+	printf("Size of a char: %lu bytes(s)\n"), (unsigned long)sizeof(d));
+	printf("Size of a int: %lu bytes(s)\n"), (unsigned long)sizeof(a));
+	printf("Size of a long int: %lu bytes(s)\n"), (unsigned long)sizeof(b));
+	printf("Size of a long long int: %lu bytes(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a float: %lu bytes(s)\n"), (unsigned long)sizeof(e));
+	return (0);
+}
