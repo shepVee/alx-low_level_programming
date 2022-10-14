00de4016251abc9e7da50847d010e769b3e78295
shepvee <shepherdmabena@gmail.com>
2022-10-14 13:33:09 -0700

    Print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints single digits
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("&d", i);
+	}
+
+	return (0);
+}
