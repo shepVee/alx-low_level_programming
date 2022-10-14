02438b704dbe85eb269253b046ce1468555a0124
shepvee <shepherdmabena@gmail.com>
2022-10-14 13:52:52 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers from 0 - 10
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	putchar(i);
+
+	return (0);
+}
