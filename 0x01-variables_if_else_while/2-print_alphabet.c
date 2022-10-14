c45e93ff0500f9ab8ba7cfad4dd569b9fea7cc3a
shepvee <shepherdmabena@gmail.com>
2022-10-14 10:29:50 -0700

    Print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 'A' ; x <= 'Z'; x++)
+	{
+		putchar(tolower(x));
+	}
+
+	return (0);
+
+}
