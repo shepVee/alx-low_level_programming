7fefc73470c093b07e7b4d2bf0610f43424bf574
shepvee <shepherdmabena@gmail.com>
2022-10-14 18:30:58 -0700

    Numbers and letters

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print hexadecimal
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+		putchar('0' + i);
+
+	for (j = 'a'; j <= 'f'; j++)
+		putchar(j);
+	putchar('\n');
+}
