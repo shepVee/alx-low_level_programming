616f120f13f2dfaf0410abfb2069a1348b790d7d
shepvee <shepherdmabena@gmail.com>
2022-10-19 15:43:00 -0700

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#inlude "main.h"
+
+/**
+ *_abs - prints absolute value of number
+ *
+ *@b: function parameter
+ *
+ *Return:Always 0 (Sucess)
+ */
+int _abs(int b)
+{
+	if (b < 0)
+	{
+		return (-b);
+	}
+	else
+	{
+		return (b);
+	}
+}
+
