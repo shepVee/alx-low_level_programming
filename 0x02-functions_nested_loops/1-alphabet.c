70f67ea129022cc34e0988a8989cb9f7a2c8e5c6
shepvee <shepherdmabena@gmail.com>
2022-10-19 12:51:01 -0700

    print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabet in lowercase
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	int a = 97;
+
+	while (a < 123)
+	{
+		_putchar(a);
+		a++;
+	}
+}
