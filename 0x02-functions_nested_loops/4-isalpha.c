912c793b77f312a6f6cab4467f39389cd6f1aee9
shepvee <shepherdmabena@gmail.com>
2022-10-19 14:43:15 -0700

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks lower and upper
+ *
+ * @c: function parameter
+ *
+ * Return: Always 1 (Sucess) and 0 (Otherwise)
+ */
+int _isalpha(int c)
+{
+	if (c >= 'a' || c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
