8a9d9b1f7af0e96b040a71d2be7e0ef93a74be52
shepvee <shepherdmabena@gmail.com>
2022-10-19 13:43:18 -0700

    print 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet x10 in small case
+ *
+ * Return: 0 (Sucess)
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 11; i++)
+	{
+		for (j = 'a'; j < 'z'; j++)
+		{
+			_putchar(j);
+		}
+	}
+
+	_putchar('\n');
+}
