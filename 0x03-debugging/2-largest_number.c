2cc98fffb5c6040011765bb4ab448240bb3901c3
shepvee <shepherdmabena@gmail.com>
2022-10-20 16:00:30 -0700

    largest number

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * largest_number - returns the largest number
+ * @a: first parameter
+ * @b: second parameter
+ * @c: third parameter
+ * Return: largest number
+ */
+int largest_number(int a, int b, int c)
+{
+	int largest;
+
+	if (a > b || b > c)
+	{
+		largest = a;
+	}
+	else if (b > a || a > c)
+	{
+		largest = b;
+	}
+	else
+	{
+		largest = c;
+	}
+
+	return (largest);
+}
