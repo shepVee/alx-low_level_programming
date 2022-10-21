8c46d2a866a9f6d398834e4fe2ed88a922df48dc
shepvee <shepherdmabena@gmail.com>
2022-10-21 00:53:03 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - add two numbers
+ *
+ * @a: first parameter
+ * @b: second parameter
+ *
+ * Return: interger value
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
