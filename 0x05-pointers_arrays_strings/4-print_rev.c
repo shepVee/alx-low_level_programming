b2109f8e3d766f1533a53e902f8c98f1570ab600
shepvee <shepherdmabena@gmail.com>
2022-10-24 12:51:48 -0700

    Print rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *print_rev - prints in reverse
+ *
+ *@s: funtion parameter
+ *
+ *Return: void
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (i >= 0)
+	{
+		if (s[i] == '\0')
+			break;
+		i++;
+	}
+
+	for (i--; i >= 0; i--)
+		_putchar(s[i]);
+
+	_putchar('\n');
+}
