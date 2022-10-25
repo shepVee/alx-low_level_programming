febabcc940df2d81cf326140bd63ffd0d4dfb681
shepvee <shepherdmabena@gmail.com>
2022-10-25 11:51:17 -0700

    stringcopy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *_strcpy - copies string to another
+ *
+ *@dest: where string is copied to
+ *@src: string being copied
+ *
+ *Return: pointer to a char
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (i >= 0)
+	{
+		dest[i] = src[i];
+		if (i == '\0')
+			break;
+
+			i++;
+	}
+}
