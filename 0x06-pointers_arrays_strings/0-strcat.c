de8d41b3481582b45bff7bf65bc0346c5ade3af9
shepvee <shepherdmabena@gmail.com>
2022-10-30 05:15:58 -0700

    copy

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -13,17 +13,17 @@ char *_strcat(char *dest, char *src)
 	int num = 0;
 	int num2 = 0;
 
-	while (dest[num] != '\0')
+	while (*(dest + num) != '\0')
 	{
 		num++;
 	}
 	while (num2 >= 0)
 	{
-		dest[num] = src[num2];
-		if (src[num2] == '\0')
-				break;
-				num++;
-				num2++;
+		*(dest + num) = *(src + num2);
+		if (*(src + num2) == '\0')
+			break;
+		num++;
+		num2++;
 	}
 	return (dest);
 }
