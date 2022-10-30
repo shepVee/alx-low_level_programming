f202b607796cc93dad8e9a9e138c8b215b4a8018
shepvee <shepherdmabena@gmail.com>
2022-10-30 07:51:21 -0700

    capitalize

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ *cap_string - capitalizes all strings
+ *
+ *@n: function parameter
+ *
+ *Return: pointer to a string
+ */
+char *cap_string(char *n)
+{
+	int c = 0;
+	int v;
+	int words[] = {92, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+
+	if (*(n + c) >= 97 && *(n + c) <= 122)
+		*(n + c) = *(n + c) - 32;
+	c++;
+	while (*(n + c) != '\0')
+	{
+		for (v = 0; v < 13; v++)
+		{
+			if (*(n + c) == words[v])
+			{
+				if ((*(n + (c + 1)) >= 97) && (*(n + (c + 1)) <= 122))
+					*(n + (c + 1)) = *(n + (c + 1)) - 32;
+				break;
+			}
+		}
+		c++;
+	}
+	return (n);
+}
