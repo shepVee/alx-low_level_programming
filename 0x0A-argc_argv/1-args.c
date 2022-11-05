e81b6a2c6886072521473b6d3b5d89a14a55ddd8
shepvee <shepherdmabena@gmail.com>
2022-11-05 11:22:45 -0700

    number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main - prints number of arguments
+ *
+ *@argc: number of arguments
+ *@argv: command line arguments
+ *
+ *Return: Always 0 (sucess)
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("argc = %d\n", argc);
+	return (0);
+}
