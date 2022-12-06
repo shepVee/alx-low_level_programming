8fbc88dd35b4bb9dd96d37bb6404e6eec4972df7
shepvee <shepherdmabena@gmail.com>
2022-12-06 11:14:47 -0800

    print binary

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -4,10 +4,10 @@
 
 int _strlen(const char *s);
 unsigned int binary_to_uint(const char *b);
-
-
-
-
+void print_binary(unsigned long int n);
+void divide(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
+int _putchar(char c);
 
 
 #endif
