0fa2c8016c7a9a88f91f783384ca5cf53e3602cc
shepvee <shepherdmabena@gmail.com>
2022-12-06 09:24:20 -0800

    Read text file

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,19 @@
+#ifndef _main_h_
+#define _main_h_
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+
+
+
+
+
+
+
+#endif
